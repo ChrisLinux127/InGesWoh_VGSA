6039e1a40914be66980d23ddebc772a8ddd6a119
ChrisLinux127 <81356792+ChrisLinux127@users.noreply.github.com>
2021-04-14 18:22:05 +0200

    Added define command for VGSA

--- vgsa_view.h
+++ vgsa_view.h
@@ -21,6 +21,24 @@
 #include<QDebug>    //Für Ausgaben in Debuger
 #include<QtWidgets> //Für Warnungen (QMessageBox
 
+//Definition of VGSA Commands for CMD Register:
+#define BROADCAST 0x00
+#define GET_GASSET_CONFIG 0x30
+#define START_VGSA 0x31
+#define STOP_VGSA 0x32
+
+#define SET_DEVICE_CONFIG 0x02
+#define GET_DEVICE_CONFIG 0x04
+#define GET_RESULT 0x09
+#define GET_TREND 0x0D
+
+#define RESET_ZERO_LEVEL 0x0B
+#define SET_GASPRINT 0x10
+#define CLR_GASPRINT 0x20
+
+#define ACK_ZERO_LEVEL 0x4B
+#define ACK_SET_GASPRINT 0x41
+#define ACK_CLR_GASPRINT 0x42
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class VGSA_View; }
@@ -42,6 +60,7 @@ private slots:
     void readSerial();
 
 private:
+
     Ui::VGSA_View *ui;  //conncetion to Formular.ui to design the gui
     QSerialPort *Controller;    //Serial Connection to Controller (VGSA)
     static const quint16 s_Controller_Vendor_ID=1027;   //Vendor ID of VGSA
